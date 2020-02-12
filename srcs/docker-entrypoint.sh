8530fbb0f99e9ff52fe39e061a5990df8099a5b6
selgrabl <selgrabl@student.42.fr>
2020-02-12 15:56:45 +0100

    avec init goinfre

--- srcs/docker-entrypoint.sh
+++ srcs/docker-entrypoint.sh
@@ -20,4 +20,4 @@ mysql < srcs_docker/data_base.sql
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt < srcs_docker/certificate
 
 service nginx restart
-/bin/sh
+/bin/bash
